80d32f317494e85865bd0a91f227d246d2fbd558
x397428 <gabrielnicolllau1@gmail.com>
2022-11-23 09:39:39 -0300

    Exercício 044 - Formas de pagamento (Concluído)

--- Ex044.py
+++ Ex044.py
@@ -10,25 +10,27 @@
 # — 3x ou mais no cartão: 20% de juros
 
 ########################################################################################################################
+from time import sleep
 
 # Layout
 print('=-' * 7, 'FORMAS DE PAGAMENTO', '-=' * 7)
 
 # Layout da formas de pagamento.
 
-print('ESCOLHA UMA FORMA DE PAGAMENTO')
-
+print('\33[33mESCOLHA UMA FORMA DE PAGAMENTO\033[m')
 print('''
-[ 1 ] à vista dinheiro/cheque
-[ 2 ] à vista cartão
-[ 3 ] 2x no cartão
-[ 4 ] 3x ou mais no cartão''')
+\033[34m[ 1 ]\033[m à vista dinheiro/cheque
+\033[35m[ 2 ]\033[m à vista cartão
+\033[36m[ 3 ]\033[m 2x no cartão
+\033[37m[ 4 ]\033[m 3x ou mais no cartão''')
 
 # Escolha do cliente e valor da compra.
 
-escolha = int(input('Qual sua escolha de pagamento ? '))
-valor = float(input('Digite o valor da compra R$: '))
+escolha = int(input('\033[33mQual sua escolha de pagamento ?\033[m '))
+valor = float(input('\033[33mDigite o valor da compra R$:\033[m '))
 
+print('\033[31mCalculando...\033[m')
+sleep(1.5)
 # à vista dinheiro/cheque: 10% de desconto
 if escolha == 1:
     desconto = valor - (valor * 10 / 100)
@@ -39,7 +41,7 @@ if escolha == 1:
 elif escolha == 2:
     desconto_cartão = valor - (valor * 5 / 100)
     print('Sua compra de \033[32mR${:.2f}\033[m pagando à vista no cartão'.format(valor), end='')
-    print(' vai sair por \033[32mR${:.2f}\033[m no final.'.format(desconto_cartão))
+    print(' vai sair por \033[32mR${:.2f}\033[m.'.format(desconto_cartão))
 
 # em até 2x no cartão: preço formal
 
@@ -50,5 +52,8 @@ elif escolha == 3:
 
 elif escolha == 4:
     vezes = int(input('Quantas vezes será parcela (limite de até 12 vezes): '))
+    print('\033[31mCalculando...\033[m')
+    sleep(1.5)
     parcelado3 = valor / vezes
-    print('Sua compra de \033[32mR${:.2f} parcelado em {}x')
+    print('Sua compra de \033[32mR${:.2f} parcelado em {}x'.format(valor, vezes), end='')
+    print(' vai sair por \033[32mR${:.2f}\033[m.'.format(parcelado3))
