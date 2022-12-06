1f7ab19cc7a657649d96188bd13f69de525e2460
Gabriel <gabrielnicolllau1@gmail.com>
2022-12-05 22:34:28 -0300

    Ex053: Dizer se a frase é um palíndromo (em andamento)

---
+++ Ex053.py
@@ -0,0 +1,12 @@
+"""Exercício Python 53: crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os
+espaços. Exemplos de palíndromos:
+
+APOS A SOPA.
+A SACADA DA CASA.
+A TORRE DA DERROTA.
+O LOBO AMA O BOLO.
+A NOTARAM A DATA DA MARATONA.
+"""
+
+frase = str(input('Digite uma frase: ')).upper().strip()
+
