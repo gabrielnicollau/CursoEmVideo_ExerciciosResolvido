5ea34f434251d428cd6d905602750199ae07de10
x397428 <gabrielnicolllau1@gmail.com>
2022-12-27 12:21:05 -0300

    Exercício 62: Super Progressão Aritmética v3.0 (concluído)

--- Ex051.py
+++ Ex051.py
@@ -9,10 +9,17 @@ print('=-' * 17)
 termo = int(input('Digite o primeiro termo: '))
 razão = int(input('Digite a razão: '))
 
-cont = 1
+mais = 10  # Limite de 10 termos.
+total = 0  # Variável para fazer a soma de termos.
+cont = 1  # Contar a quantidade de loop's.
 
-while cont <= 10:
-    print('\033[32m{}\033[m \033[34m--> \033[m'.format(termo), end='')
-    cont += 1
-    termo += razão
+while mais != 0:  # Enquanto mais não receber o input com valor 0, vai ficar em loop perguntado qual o valor.
+    total = total + mais
+
+    while cont <= total:  # Cont sempre vai ser menor que total(total vale 10), vai ficar em loop até 'mais' receber zero., cont vai ficar maior.
+        print('\033[32m{}\033[m \033[34m--> \033[m'.format(termo), end='')
+        cont += 1  # Vai contar +1 para cada loop.
+        termo += razão  # razão vai ser somado ao termo em cada loop, ou seja, termo = termo(variável) + razão (valor fixo).
+    print('\033[33mPAUSA...\033[m')
+    mais = int(input('MAis quantos termos você gostaria de ver ? '))
 print('\033[33mFIM!\033[m')
