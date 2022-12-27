3a46066cbc5bc734fd89b427eb8ad8837561d795
x397428 <gabrielnicolllau1@gmail.com>
2022-12-27 12:19:55 -0300

    Exercício 61 (while): Progressão Aritmética v2.0 (concluído)

--- Ex051.py
+++ Ex051.py
@@ -9,17 +9,10 @@ print('=-' * 17)
 termo = int(input('Digite o primeiro termo: '))
 razão = int(input('Digite a razão: '))
 
-mais = 10  # Limite de 10 termos.
-total = 0  # total (0) + mais (10).
-cont = 1  # Contar a quantidade de loop's.
+cont = 1
 
-while mais != 0:  # Enquanto mais não receber o input com valor 0, vai ficar em loop perguntado qual o valor.
-    total = total + mais
-
-    while total >= cont:  # Cont sempre vai ser menor que total(total vale 10), vai ficar em loop até 'mais' receber zero., cont vai ficar maior.
-        print('\033[32m{}\033[m \033[34m--> \033[m'.format(termo), end='')
-        cont += 1  # Vai contar +1 para cada loop.
-        termo += razão  # razão vai ser somado ao termo em cada loop, ou seja, termo = termo(variável) + razão (valor fixo).
-    print('\033[33mPAUSA...\033[m')
-    mais = int(input('Mais quantos termos você gostaria de ver ? '))
+while cont <= 10:
+    print('\033[32m{}\033[m \033[34m--> \033[m'.format(termo), end='')
+    cont += 1
+    termo += razão
 print('\033[33mFIM!\033[m')
