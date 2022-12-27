f03324a1d17dc12f39320c02970315c27ebf17fc
x397428 <gabrielnicolllau1@gmail.com>
2022-12-27 12:14:33 -0300

    Exercício 62 (while): Super Progressão Aritmética v3.0 (concluído)

--- Ex051.py
+++ Ex051.py
@@ -2,15 +2,24 @@
 termos da progressão usando a estrutura while."""
 
 
-print('=-' * 12)
+print('=-' * 17)
 print('\033[33mOS 10 PRIMEIROS TERMOS DE UMA PA\033[m')
-print('=-' * 12)
+print('=-' * 17)
 
 termo = int(input('Digite o primeiro termo: '))
 razão = int(input('Digite a razão: '))
-limite = 1  # Limite começa com um para fazer os 10 primeiros termos, se fosse zero seria 11.
 
-while limite <= 10:  # ‘Loop’ vai até 10.
-    print('{} -> '.format(termo), end='')  # Mostrar o primeiro termo.
-    termo += razão  # o termo será somada a razão em cada loop, ou seja, vai aumentando o valor em cada volta.
-    limite += 1  # Limite do loop.
+mais = 10  # Limite de 10 termos.
+total = 0  # total (0) + mais (10).
+cont = 1  # Contar a quantidade de loop's.
+
+while mais != 0:  # Enquanto mais não receber o input com valor 0, vai ficar em loop perguntado qual o valor.
+    total = total + mais
+
+    while total >= cont:  # Cont sempre vai ser menor que total(total vale 10), vai ficar em loop até 'mais' receber zero., cont vai ficar maior.
+        print('\033[32m{}\033[m \033[34m--> \033[m'.format(termo), end='')
+        cont += 1  # Vai contar +1 para cada loop.
+        termo += razão  # razão vai ser somado ao termo em cada loop, ou seja, termo = termo(variável) + razão (valor fixo).
+    print('\033[33mPAUSA...\033[m')
+    mais = int(input('Mais quantos termos você gostaria de ver ? '))
+print('\033[33mFIM!\033[m')
