fea32fdadb886f89f16562551badbbc3a9e3731b
x397428 <gabrielnicolllau1@gmail.com>
2022-12-28 15:29:25 -0300

    Exercício 63: Sequência de Fibonacci v1.0 (concluído)

--- Ex063(while-fibonacci_v1.0).py
+++ Ex063(while-fibonacci_v1.0).py
@@ -1,24 +1,29 @@
 """Exercício Python 63: Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros
 elementos de uma Sequência de Fibonacci. Exemplo:"""
 
-print('-' * 23)
+# Para saber o proximo termo, basta somar o penultimo e ultimo termo da sequência.
+# Prox_termo = penultimo + ultimo. O valor dessa soma é o proximo termo.
+# 0 - 1 - 1 - 2 - 3 - 5 - 8
+# 0 + 1 = 1
+# 1 + 1 = 2
+# 2 + 1 = 3
+
+print('-' * 55)
 print('\033[34mSequência de Fibonacci\033[m')
-print('-' * 23)
+print('-' * 55)
 
-termo = int(input('aqui: '))
+termo = int(input('Digite quantos termos você quer ver: '))
 
-penultimo = 0  # Primeiro valor na sequência de fibonacci.
-ultimo = 1  # Segundo valor na sequência de fibonacci.
-cont = 3
+penultimo = 0  # Primeiro termo da sequência.
+ultimo = 1  # Segundo termo da sequência.
 
-print('{} --> {}'.format(penultimo, ultimo), end='')
+print('\033[31m{} ➡ {} \033[m'.format(penultimo, ultimo), end='')
 
+cont = 3  # Contador de termos, inicia no 3 porque já temos os dois primeiro termos (0 e 1 são os dois primeiro padrões).
 while cont <= termo:
-    prox_sequencia = ultimo + penultimo
-    print('{} --> '.format(prox_sequencia), end='')
-
-    penultimo = ultimo
-    ultimo = prox_sequencia
-    cont += 1
-
-print('\033[33mFIM!\033[m')
+    prox_termo = penultimo + ultimo  # Variável prox_termo: Para descobrir o proximo termo na sequência temos que somar os dois anteriores.
+    penultimo = ultimo  # Quando a primeira sequência se formar, o último termo vai virar penultimo termo.
+    ultimo = prox_termo  # E o proximo termo vai virar o último.
+    cont += 1  # Encerrar o loop.
+    print('\033[32m➡ {} \033[m'.format(prox_termo), end='')
+print('\33[33m➡ FIM!\033[m')
